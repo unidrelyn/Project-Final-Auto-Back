b7f4637e36c057fe51262074bb84ecaff8e7728a
unknown <unidrelyn@gmail.com>
2024-04-03 17:00:03 +0200

    firts db

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
-require("dotenv").config();
-const jsonServer = require("json-server");
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
 const morgan = require("morgan");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const auth = require("./routes/auth.routes");
+const cars = require("./db.json");
 
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
-const PORT = process.env.PORT || 5002;
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(auth);
 
-server.use(middlewares);
-server.use(morgan("dev"));
-server.use((req, res, next) => {
-  // Middleware to disable CORS
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
-server.use(router);
+const errorHandler = (err, req, res, next) => {
+	console.error(err); // Log the error for debugging purposes
+	res.status(500).json({ error: "Internal Server Error" }); // Send an appropriate error response to the client
+};
 
-server.listen(PORT, () => {
-  console.log(`JSON Server is running at port ${PORT}`);
-});
+app.use(errorHandler);
+
+mongoose
+	.connect("mongodb://127.0.0.1:27017")
+	.then((x) =>
+		console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+	)
+	.catch((err) => {
+		console.error("Error connecting to mongo", err);
+	});
+
+app.use(
+	cors({
+		origin: ["http://localhost:5173", "http://localhost:5005"],
+	})
+);
+
+app.get("/api/cars", (req, res) => res.json(cars));
---
+++ models/user.model.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+  email: { type: "string", required: true, unique: true },
+  password: { type: "string", required: true },
+  name: { type: "string", required: true },
+});
+
+module.exports = model("User", userSchema);
