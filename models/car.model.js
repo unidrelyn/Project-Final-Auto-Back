d8358817050f3e616b9ec7b9c529bebcd6d7278b
unknown <unidrelyn@gmail.com>
2024-04-10 22:23:16 +0200

    img default

--- models/car.model.js
+++ models/car.model.js
@@ -14,7 +14,11 @@ const carSchema = new Schema({
 	transmission: { type: String, enum: ["manual", "automatic"] },
 	year: { type: Number },
 	color: { type: String },
-	image: { type: String },
+	image: {
+		type: String,
+		default:
+			"https://media.istockphoto.com/id/1325694210/es/vector/sed%C3%A1n-car-line-art-vector-icon-monochrome-illustration.jpg?s=612x612&w=0&k=20&c=dapCTaL8GvkOB53yeWOdjyRhmseoEe_JBysWrrc5OIo=",
+	},
 });
 
 const Car = mongoose.model("Car", carSchema);
